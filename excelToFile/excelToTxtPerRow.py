cc61b4b2eb051d0bcfed62db870704dd5b933684
Han Guolong <wojiaohgl@126.com>
2015-07-24 11:16:20 +0800

    Signed-off-by: Han Guolong <wojiaohgl@126.com>

---
+++ excelToFile/excelToTxtPerRow.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+import csv
+csvfile = file('day6.10  after order.csv', 'rb')
+reader = csv.reader(csvfile)
+i=0
+for line in reader:
+	f=open(str(i)+'.txt','w')
+	j=0
+	for elem in line:
+		if(j!=0):
+			f.write(',')
+		f.write(elem)
+		j+=1
+	i+=1
+	f.close()
+	#print type(line)
+csvfile.close() 
+
